f7d85c8fad2cbefb1f37f9a85265ccfb55611ced
GO672 <109583504+GO672@users.noreply.github.com>
2025-06-21 14:31:27 +0700

    Update apiService.test.js

--- Tests/CartTests/apiService.test.js
+++ Tests/CartTests/apiService.test.js
@@ -1,1 +1,26 @@
+describe('API Header Data Clumps Tests', () => {
+  const testToken = "test-token-123";
 
+  beforeEach(() => {
+    localStorage.getItem.mockReturnValue(testToken);
+  });
+
+  const verifyHeaders = (headers) => {
+    expect(headers).toEqual({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${testToken}`
+    });
+  };
+
+  test('addToCart headers', async () => {
+    await addToCart(1);
+    const call = fetch.mock.calls[0][1];
+    verifyHeaders(call.headers);
+  });
+
+  test('createOrder headers', async () => {
+    await createOrderButton.click();
+    const call = fetch.mock.calls[0][1];
+    verifyHeaders(call.headers);
+  });
+});
